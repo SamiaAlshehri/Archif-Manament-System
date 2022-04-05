9e4683371d4f9c1227504d34579763317f9ebbda
frappe <frappe@ubuntu>
2022-04-05 19:56:27 +0000

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Archif Managment System
+
+this app mange your data and arrang it in simple way and make category for all kind of your data or books
+
+#### License
+
+MIT
---
+++ archif_managment/config/desktop.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Archif Managment System",
+			"color": "lightblue",
+			"icon": "octicon octicon-file-directory",
+			"type": "module",
+			"label": _("Archif Managment System")
+		}
+	]
